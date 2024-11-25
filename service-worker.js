47bcc7be968c23a64f69235bad051638140599e8
Sena Akbulut Kurçenli <34038741+Senakbulut@users.noreply.github.com>
2024-11-25 15:26:53 +0300

    Update service-worker.js
    package url changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.0b6490429e4de18ccbc92d41a4799605.js"
+  "/precache-manifest.4b0516f1abb1f83cdfd0492bb8316ae6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
-  
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
