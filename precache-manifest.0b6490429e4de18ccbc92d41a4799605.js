72c7459a769958e523f1d5544999f4a912623390
Sena Akbulut <Senakbulut@users.noreply.github.com>
2024-11-25 14:51:40 +0300

    initial commit

---
+++ precache-manifest.0b6490429e4de18ccbc92d41a4799605.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2d1429ec981ef1d530575512bf147f",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "ce1591f68d79ed629fb6",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "09773399a958f18018d5",
+    "url": "/react-frontend-dev-portfolio/static/css/main.61898b3d.chunk.css"
+  },
+  {
+    "revision": "ce1591f68d79ed629fb6",
+    "url": "/react-frontend-dev-portfolio/static/js/2.d1b4bedb.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.d1b4bedb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09773399a958f18018d5",
+    "url": "/react-frontend-dev-portfolio/static/js/main.9fc5c9c9.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  },
+  {
+    "revision": "f4478d537ef0a8abc6924526650a8583",
+    "url": "/react-frontend-dev-portfolio/static/media/myProfile.f4478d53.jpg"
+  },
+  {
+    "revision": "bb64e91ecfde92dd809b484a97e5d978",
+    "url": "/react-frontend-dev-portfolio/static/media/project.bb64e91e.jpg"
+  }
+]);
