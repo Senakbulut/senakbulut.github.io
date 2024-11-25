9557e7ea6bf2cd9bc9457fb49a5e430787b5c4ac
Sena Akbulut Kurçenli <34038741+Senakbulut@users.noreply.github.com>
2024-11-25 15:32:44 +0300

    Update precache-manifest.0b6490429e4de18ccbc92d41a4799605.js
    package url changes

--- precache-manifest.0b6490429e4de18ccbc92d41a4799605.js
+++ precache-manifest.0b6490429e4de18ccbc92d41a4799605.js
@@ -1,38 +1,38 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7e2d1429ec981ef1d530575512bf147f",
-    "url": "/react-frontend-dev-portfolio/index.html"
+    "revision": "82157750c3e278f20b292d35403df5a9",
+    "url": "/index.html"
   },
   {
-    "revision": "ce1591f68d79ed629fb6",
-    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+    "revision": "e98ed4303e53d5e663b3",
+    "url": "/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "09773399a958f18018d5",
-    "url": "/react-frontend-dev-portfolio/static/css/main.61898b3d.chunk.css"
+    "revision": "eeb0c6a4f103aed3ae93",
+    "url": "/static/css/main.61898b3d.chunk.css"
   },
   {
-    "revision": "ce1591f68d79ed629fb6",
-    "url": "/react-frontend-dev-portfolio/static/js/2.d1b4bedb.chunk.js"
+    "revision": "e98ed4303e53d5e663b3",
+    "url": "/static/js/2.3a902668.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "/react-frontend-dev-portfolio/static/js/2.d1b4bedb.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.3a902668.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "09773399a958f18018d5",
-    "url": "/react-frontend-dev-portfolio/static/js/main.9fc5c9c9.chunk.js"
+    "revision": "eeb0c6a4f103aed3ae93",
+    "url": "/static/js/main.1f666359.chunk.js"
   },
   {
-    "revision": "8b91e79b160a90e30d4f",
-    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+    "revision": "06f3364048aae3508943",
+    "url": "/static/js/runtime-main.b7665341.js"
   },
   {
     "revision": "f4478d537ef0a8abc6924526650a8583",
-    "url": "/react-frontend-dev-portfolio/static/media/myProfile.f4478d53.jpg"
+    "url": "/static/media/myProfile.f4478d53.jpg"
   },
   {
     "revision": "bb64e91ecfde92dd809b484a97e5d978",
-    "url": "/react-frontend-dev-portfolio/static/media/project.bb64e91e.jpg"
+    "url": "/static/media/project.bb64e91e.jpg"
   }
 ]);
